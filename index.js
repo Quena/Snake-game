f4e8cf32b29adab09f49dc3dd617ecba445c281d
quena <quena.u.s@gmail.com>
2021-04-20 10:40:03 +0200

    create grid and add snake

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const grid = document.querySelector(".grid")
 const startButton = document.getElementById("start")
 const score = document.getElementById("score")
+let squares = []
+let currentSnake = [2,1,0]
+
+function createGrid() {
+    for(let i = 0; i < 100; i++) { //loop to create 100 elements
+        const square = document.createElement("div") //creates element
+        
+        square.classList.add("square")
+        grid.appendChild(square) //places the element into the grid
+        squares.push(square)//push the individual square into the squares array
+        }
+    }
+    createGrid()
+    
+   currentSnake.forEach(index => squares[index].classList.add("snake"))
