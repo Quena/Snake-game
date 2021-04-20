5b909cd8eec8c2ead6e00448b01db9a203a971e8
quena <quena.u.s@gmail.com>
2021-04-20 10:47:08 +0200

    starting the moveSnake() function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const startButton = document.getElementById("start")
 const score = document.getElementById("score")
 let squares = []
 let currentSnake = [2,1,0]
+let direction = 1
 
 function createGrid() {
     for(let i = 0; i < 100; i++) { //loop to create 100 elements
@@ -16,3 +17,12 @@ function createGrid() {
     createGrid()
     
    currentSnake.forEach(index => squares[index].classList.add("snake"))
+
+   function moveSnake() {  
+
+    const tail = currentSnake.pop() //removes last element from the array and stores it in a const
+    squares[tail].classList.remove("snake") //removes styling from the last element
+    currentSnake.unshift(currentSnake[0] + direction) //adds square in direction we are heading
+    squares[currentSnake[0]].classList.add("snake")
+}
+moveSnake()
